98cb0a383bc85f964f6f1c874dbb39faad7d4994
Bstijn <baltessen.stijn@gmail.com>
2020-06-17 10:56:23 +0200

    init

---
+++ StoryService/Settings/MessagequeueSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoryService.Settings
+{
+    public class MessagequeueSettings
+    {
+        public string Uri { get; set; }
+        public string Exchange { get; set; }
+    }
+}
